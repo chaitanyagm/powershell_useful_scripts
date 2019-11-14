2e533edd596f75d92cb34fc72be23666ec7b56cd
chaitanyagm <55421191+chaitanyagm@users.noreply.github.com>
2019-11-15 00:32:45 +1100

    Create BuildNDeploySSISProject.ps1

---
+++ BuildNDeploySSISProject.ps1
@@ -0,0 +1,3 @@
+git clone https://user:token@dev.azure.com/proj/test/_git/test
+
+&"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv.com" "C:\pathToslnFile.sln"  /Rebuild
