06a1b3b5de35effb9d4e1106464da7dba1a84a7d
chaitanyagm <55421191+chaitanyagm@users.noreply.github.com>
2019-11-15 00:34:15 +1100

    Update BuildNDeploySSISProject.ps1

--- BuildNDeploySSISProject.ps1
+++ BuildNDeploySSISProject.ps1
@@ -1,3 +1,5 @@
 git clone https://user:token@dev.azure.com/proj/test/_git/test
 
 &"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv.com" "C:\pathToslnFile.sln"  /Rebuild
+
+#https://docs.microsoft.com/en-us/visualstudio/ide/reference/rebuild-devenv-exe?view=vs-2019
